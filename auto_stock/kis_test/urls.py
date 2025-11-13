b72710ae49a4bf47133bbb1f0d6e7eea640c11ac
choma45 <phw2whp@gmail.com>
2025-11-13 12:42:04 +0900

    Docs: 삭제 예정 url 명시

--- auto_stock/kis_test/urls.py
+++ auto_stock/kis_test/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import RealtimeSymbolView, RealtimeQuoteView, DailyPriceView, TokenStatusView
 
 urlpatterns = [
-    path("symbols/", RealtimeSymbolView.as_view(), name="kis-realtime-symbols"),
+    path("symbols/", RealtimeSymbolView.as_view(), name="kis-realtime-symbols"), # 불필요한 기능, 삭제 예정
     path("quotes/", RealtimeQuoteView.as_view(), name="kis-realtime-quotes"), # 테스트 필요 
     path("daily/", DailyPriceView.as_view(), name="kis-daily-price"),
     path("token/", TokenStatusView.as_view(), name="kis-auth-token-status"),
