504c90bcb34a69f9c28a0d0f82b3c62ede61af81
choma45 <phw2whp@gmail.com>
2025-12-18 01:43:25 +0900

    Fix: WS url수정

--- auto_stock/data/routing.py
+++ auto_stock/data/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .consumers import IndicesConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/indices/$', IndicesConsumer.as_asgi()),
+    re_path(r'ws/index/$', IndicesConsumer.as_asgi()),
 ]
